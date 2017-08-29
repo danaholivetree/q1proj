60fb9c0c35c1c616c0b0d5f088eb697f59a3d92e
danah olivetree <danah.olivetree@gmail.com>
2017-08-29 11:34:44 -0600

    added shake button

--- boggle.js
+++ boggle.js
@@ -15,56 +15,92 @@ $(document).ready(function() {
   let val = 4
   let timeVal = 3
   let lengthVal = 4
+  let splitCubes=[]
 
 
   $('#gridSelect button').click(function selectGridSize(e) {
+    e.preventDefault()
+    console.log('grid selected')
     tar = e.target
     // $('#gridSelect').children().removeClass("blue lighten-4")
     // $(tar).parent().addClass("blue lighten-4")
     val = $(tar).attr('data-value')
     if (val == 4) {
       makeCubeArrays(fourCubes)
+      console.log("four by four cube arrays made")
     } else if (val == 5) {
       makeCubeArrays(fiveCubes)
+      console.log("five by five cube arrays made")
     }
     // else if (val === 6) {
     //   makeCubeArrays(sixCubes)
     // }
-    fillGrid(val)
+    // fillGrid(val)  //this works
 
     //START TIMER - should go on "shake button"
   })
 
   $('#timeSelect').click(function selectTimer(e) {
+    e.preventDefault()
     timeVal = $(e.target).attr('data-value')
 
   })
 
   $('#lengthSelect').click(function selectLength(e) {
+    e.preventDefault()
     lengthVal = $(e.target).attr('data-value')
-    
   })
+
+  $('#timer').text = timeVal + ":" + 00;
+
+  function startTimer() {
+    var presentTime = $('timer').text();
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if(s==59){m=m-1}
+    //if(m<0){alert('timer completed')}
+    $('timer').text(m + ":" + s)
+    //setTimeout(startTimer, 1000)
+  }
+  function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+    if (sec < 0) {sec = "59"};
+    return sec;
+  }
 
   function makeCubeArrays(list) {
     let cubes = list.toUpperCase().split(" ")
-    let splitCubes = cubes.map(function(el) {
+
+    splitCubes = cubes.map(function(el) {
       return el.split("")
     })
-    shake(splitCubes)
+    console.log(splitCubes)
+    //shake(splitCubes)
   }
 
+  $('#shake').click(function(){
+
+    shake(splitCubes)
+  })
+
   function shake(it) {
+
     let mixCubes = []
     while (it.length > 0) {
       mixCubes.push(it.splice(Math.floor((it.length) * Math.random()), 1))
     }
+
     ltrs = mixCubes.map(function(el) {
       return el[0][Math.floor(6 * Math.random())]
     })
-    // fillGrid(Math.sqrt(it.length))
+
+    console.log(Math.sqrt(ltrs.length))
+    fillGrid(Math.sqrt(ltrs.length))
   }
 
   function fillGrid(val) {
+    console.log('filling grid')
     $('#grid').children().remove()
     for (let i = 0; i < Math.pow(val, 2); i++) {
       let slot = $('<div>')
@@ -73,6 +109,7 @@ $(document).ready(function() {
       slot.text(ltrs[i])
     }
     $("#target").focus();
+    startTimer();
   }
 
   function makeNeighborhood(x, y, board) {
